ff77c29c6e50ddccca9052edfc21c9d827e6f46b
Matheus <127268665+Matheus2Alves@users.noreply.github.com>
2023-04-12 18:25:41 -0300

    exxjs

---
+++ aula011/exx008.js
@@ -0,0 +1,10 @@
+var vel = 40.5
+var maxvel = 50
+
+console.log(`a sua velocidade é ${vel}Km/h`)
+
+if (vel > maxvel){
+    console.log(`você ultrapassou a velocidade máxima permitida, MULTADO!`)  
+
+}  
+console.log(`sempre diriga com cinto de segurança.`)
---
+++ aula011/exx009.js
@@ -0,0 +1,7 @@
+var pais = 'brasil'
+
+if (pais == 'estados unidos'){
+    console.log(`welcome to my website!`)
+} else {
+    console.log(`bem vindo ao meu site chapa`)
+}
