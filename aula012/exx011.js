3b01f9cca998437851b03456e2e6a3bc3f44b5d9
Matheus <127268665+Matheus2Alves@users.noreply.github.com>
2023-04-13 17:39:37 -0300

    age js

---
+++ aula012/exx011.js
@@ -0,0 +1,10 @@
+var idade = 19
+if (idade > 18){
+    console.log('você é adulto')
+} else {
+    if (idade < 12) {
+        console.log('você é criança')    
+    } else {
+        console.log('você é adolescente')
+    }
+}
