523758d7131bd7fc1471ab81683eab269d7cc2f7
Matheus <127268665+Matheus2Alves@users.noreply.github.com>
2023-04-13 18:05:22 -0300

    dd

--- aula012/hora.js
+++ aula012/hora.js
@@ -1,7 +1,7 @@
-var hora = 19
+var hora = 14
 if (hora > 12 && hora < 18) {
     console.log('Boa tarde')
-} else if (hora > 18) {
+} else if (hora >= 18) {
         console.log('Boa noite')
 } else {
         console.log('Bom dia')
