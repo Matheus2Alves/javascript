675035f3cdc9277cdc02356e3bd4206dfa57d36b
Matheus <127268665+Matheus2Alves@users.noreply.github.com>
2023-04-13 18:43:23 -0300

    switch default nationalite

--- aula011/dss001.js
+++ aula011/dss001.js
@@ -2,9 +2,26 @@ function clicar() {
     var nal = document.querySelector('input#nal')
     var res = document.querySelector('div#res') 
     var nacionalidade = (nal.value)
-    if (nacionalidade != 'BR') {
-        res.innerHTML = `<p>Você é estrangeiro</p>`
-    } else {
-        res.innerHTML = `<p>Você é brasileiro</p>`
+    switch (nacionalidade) {
+        case 'BR':
+            res.innerHTML = 'Você é Brasileiro'
+            break
+        case 'Brasil':
+            res.innerHTML = 'Você é Brasileiro'
+            break
+        case 'brasil':
+            res.innerHTML = 'Você é Brasileiro'
+            break
+        case 'brazil':
+            res.innerHTML = 'Você é Brasileiro'
+            break
+        case 'Brazil':
+            res.innerHTML = 'Você é Brasileiro'
+            break
+        case 'br':
+            res.innerHTML = 'Você é Brasileiro'
+            break
+        default:
+            res.innerHTML = 'Você é estrangeiro'
     }
-}
+}    
