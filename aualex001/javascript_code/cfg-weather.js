0cdeb3f4837d54baad7606fb4f016302411a1e17
Matheus <127268665+Matheus2Alves@users.noreply.github.com>
2023-04-14 17:59:56 -0300

    weather web site

--- aualex001/javascript_code/cfg-weather.js
+++ aualex001/javascript_code/cfg-weather.js
@@ -1,10 +1,19 @@
-//var horário_do_sistema = new Date()
-//var horário = horário_do_sistema.getHours()
+var horário_do_sistema = new Date()
+var horário = horário_do_sistema.getHours()
 var horário_texto = document.querySelector('#time')
 var imagem_do_clima = document.querySelector('#image-time')
 var main = document.querySelector('#principal')
-var horário = 8
+var body = document.querySelector('body')
+
 
 if (horário < 12 && horário > 5) {
-    main.style.background = 'red'
-}
+    main.style.backgroundColor = '#a0e96e'
+    body.style.backgroundColor = '#c3e767'
+    horário_texto.innerHTML = `são <strong>${horário}</strong> da manhã`
+    imagem_do_clima.style.backgroundImage = 'url("./imagens/sunrise_landscape.jpg")'
+} else if(horário >= 12 && horário < 18) {
+    main.style.backgroundColor = '#C77950'
+    body.style.backgroundColor = '#E06522'
+    horário_texto.innerHTML = `são <strong>${horário}</strong> da tarde`
+    imagem_do_clima.style.backgroundImage = 'url("./imagens/midday_landscape.png")'
+} 
