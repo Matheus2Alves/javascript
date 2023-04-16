f94f1334ed9cda07709a8292a7317827512fc599
Matheus <127268665+Matheus2Alves@users.noreply.github.com>
2023-04-16 12:37:00 -0300

    age_checker_site_js

---
+++ aualex001/age_checker/age_checker/cfgage.js
@@ -0,0 +1,11 @@
+var age = querySelector('#age')
+var M = querySelector('#sex1')
+var F = querySelector('#sex2')
+var calcular = querySelector('#enviar')
+var texto_idade = querySelector('h2')
+var imagem_idade = querySelector('#image')
+calcular.addEventListener('click', clicar)
+
+function clicar() {
+    
+}
