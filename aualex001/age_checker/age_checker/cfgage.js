dbdce812c417c40be7941efa24e42a8b3ef3c19e
Matheus <127268665+Matheus2Alves@users.noreply.github.com>
2023-04-16 21:32:24 -0300

    update

--- aualex001/age_checker/age_checker/cfgage.js
+++ aualex001/age_checker/age_checker/cfgage.js
@@ -2,10 +2,10 @@ var age = querySelector('#age')
 var M = querySelector('#sex1')
 var F = querySelector('#sex2')
 var calcular = querySelector('#enviar')
-var texto_idade = querySelector('h2')
+var texto_idade = querySelector('#texto')
 var imagem_idade = querySelector('#image')
 calcular.addEventListener('click', clicar)
 
 function clicar() {
-    
+    texto_idade.innerHTML = (`ola ${age}`)
 }
