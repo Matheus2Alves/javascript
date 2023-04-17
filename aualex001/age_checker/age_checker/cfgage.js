321e979ddbd90aa869b198ab4c08d8ce9a382048
Matheus <127268665+Matheus2Alves@users.noreply.github.com>
2023-04-17 19:42:40 -0300

    big problem

--- aualex001/age_checker/age_checker/cfgage.js
+++ aualex001/age_checker/age_checker/cfgage.js
@@ -1,11 +1,19 @@
-var age = querySelector('#age')
-var M = querySelector('#sex1')
-var F = querySelector('#sex2')
-var calcular = querySelector('#enviar')
-var texto_idade = querySelector('#texto')
-var imagem_idade = querySelector('#image')
-calcular.addEventListener('click', clicar)
+var age = document.querySelector('#age')
+var M = document.querySelector('#sex1')
+var F = document.querySelector('#sex2')
+var calcular = document.querySelector('#enviar')
+var texto_idade = document.querySelector('#texto')
+var imagem_idade = document.querySelector('#image')
+var realage = parseInt(age.value)
+var data = new Date()
+var ano_atual = data.getFullYear()
+var realyear = parseInt(ano_atual)
 
-function clicar() {
-    texto_idade.innerHTML = (`ola ${age}`)
-}
+calcular.addEventListener('click', function() {
+    //var sua_idade = parseInt(realage - ano_atual)
+    texto_idade.innerHTML = `${realage}`
+    if(sua_idade < 12 && sua_idade > 0) {
+        texto_idade.innerHTML = `Você é uma criança, e tem ${sua_idade} anos`
+    }    
+})
+    
