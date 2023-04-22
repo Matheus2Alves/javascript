770aaa5cb975b9f8496e1c456155516b688ef18f
Matheus <127268665+Matheus2Alves@users.noreply.github.com>
2023-04-22 15:59:06 -0300

    arrays pt2

--- array_na_tela.js
+++ array_na_tela.js
@@ -1,5 +1,9 @@
 let nomes = [ 'matheus', 'aline', 'giliard', 'saulo']
 console.log(`array origi ${nomes}`)
-for(let pos = 0; pos < nomes.length; pos++) {
+/*for(let pos = 0; pos < nomes.length; pos++) {
+    console.log(`a posição ${pos} tem o dado ${nomes[pos]}`)
+}*/
+
+for (let pos in nomes) {
     console.log(`a posição ${pos} tem o dado ${nomes[pos]}`)
 }
