ea1532a283dd9c75c255268f4194f57e1bf209a2
Matheus <127268665+Matheus2Alves@users.noreply.github.com>
2023-04-24 23:26:42 -0300

    analytic arrays

--- exfinal/ex07/ananum.js
+++ exfinal/ex07/ananum.js
@@ -23,7 +23,26 @@ function yesList(n, valores) {
 
 function envia() {
     if (yesNumber(txtnum.value) && !yesList(txtnum.value, valores)) {
-        window.alert('boa')
+        let selcont = document.createElement('option')
+        selcont.text = `O valor ${txtnum.value} foi inserido`
+        seltab.appendChild(selcont)
+        valores.push(txtnum.value)
+        txtnum.value = ''
+        let finalizar = document.querySelector('#finalizar')
+        finalizar.addEventListener('click', final)
+        function final() {
+            let menor = Math.min(...valores)
+            let soma = 0
+            for (let pos = 0; pos < valores.length; pos++) {
+                 soma += Number(valores[pos])
+            }
+            let media = Number(soma) / Number(valores.length)
+            res.innerHTML = `Ao todo, temos ${valores} números cadastrados` 
+            res.innerHTML += `<br> O menor valor informado foi ${menor}`
+            res.innerHTML += `<br>Somando todos os valores o resultado é ${soma}`
+            res.innerHTML += `<br>a média dos valores é ${media}`
+        } 
+        
     } else {
         window.alert('o seu valor precisa ser um número e não pode já estar inserido na lista!')
     }
