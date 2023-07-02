ac391a9bc96cd36a1ede5d8252241e0193570146
Matheus <127268665+Matheus2Alves@users.noreply.github.com>
2023-07-02 17:48:20 -0300

    revise

--- v2jsrevised/tabu/tabuj.js
+++ v2jsrevised/tabu/tabuj.js
@@ -2,12 +2,17 @@ window.onload = function () {
     let calcTab = document.getElementsByTagName('input')[1]
     calcTab.addEventListener('click', function() {
         let numTab = document.querySelector('input#numtab')
-        let num = Number(numTab.value)
-        let restabu = document.querySelector('textarea#restabu')
-        restabu.innerHTML = ''
-        for(let i = 0; i <= 10; i++) {
-            let resNum = num * i
-            restabu.innerHTML += num + 'x' + i + '=' + resNum + '\n' 
+        if(numTab.value.length == 0) {
+            restabu.innerHTML = 'digite um número para realizar a tabuada!'
+        } else {
+            let num = Number(numTab.value)
+            let restabu = document.querySelector('textarea#restabu')
+            restabu.innerHTML = ''
+            for(let i = 0; i <= 10; i++) {
+                let resNum = num * i
+                restabu.innerHTML += num + 'x' + i + '=' + resNum + '\n' 
+            }
         }
     })
 }
+
